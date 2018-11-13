3c44bb1259fa489546f1a71d0476df29116fec3f
Key <keyla@moogah.com>
2018-11-13 13:57:23 -0300

    added vitt_receipts_web

---
+++ odoo-argentina-vitt/vitt_receipts_web/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Receipts WEB",
+    'summary': """Receipts WEB""",
+    'description': """Receipts WEB""",
+    'author': "Moogah",
+    'website': "http://www.Moogah.com",
+    'category': 'Uncategorized',
+    'version': '10.0.1.0.1',
+    'depends': [
+        'account_accountant',
+        'account_payment_group_document',
+        'account_payment_group'
+    ],
+    'data': [
+        'views/views.xml',
+    ],
+    'demo': [],
+    'application': True,
+}
