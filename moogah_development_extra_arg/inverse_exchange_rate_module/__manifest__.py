765a95e98abfc0b5653740fb68fbf218ef4674b9
Key <keyla@moogah.com>
2018-12-12 16:33:33 -0300

    change

--- moogah_development_extra_arg/inverse_exchange_rate_module/__manifest__.py
+++ moogah_development_extra_arg/inverse_exchange_rate_module/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Inverse Exchange Rate",
-    "version": "10.0.1.0.13",
+    "version": "10.0.1.0.14",
     "depends": ['base', 'account', 'account_accountant'],
     "author": "BrowseInfo",
     "description": """
